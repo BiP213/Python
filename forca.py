ab2ae7baaf763cf46ba616f9883eec6cdf8d6cac
Feliphe M. Pereira <feliphemickael@proton.me>
2022-10-09 17:24:46 -0300

    Refatorando o código

--- forca.py
+++ forca.py
@@ -2,10 +2,51 @@ import random
 
 
 def jogar():
+
+    mensagem_boas_vindas()
+    palavra_secreta = carrega_palavra_secreta()
+
+    letras_acertadas = inicializa_letras_acertadas(palavra_secreta)
+    # array, lista com as letras acertadas pelo usuário
+    # cria uma lista e
+    # adiciona dinamicamente de acordo com o tamanho da palavra
+    # Funcionalidade List Comprehensions
+    print(letras_acertadas)
+
+    erros = 0
+
+    while (True):
+        chute = pede_chute()
+
+        if (chute in palavra_secreta):
+            acertou_chute(chute, letras_acertadas, palavra_secreta)
+
+        else:
+            erros += 1  # erros = erros + 1
+            desenha_forca(erros)
+
+        if (erros == 7):
+            break
+        if ("_" not in letras_acertadas):
+            print(letras_acertadas)
+            break
+
+        print(letras_acertadas)
+
+    if ("_" not in letras_acertadas):
+        imprime_mensagem_vitoria()
+
+    else:
+        imprime_mensagem_derrota(palavra_secreta)
+
+
+def mensagem_boas_vindas():
     print("*********************************")
     print("***Bem-vindo ao jogo da Forca!***")
     print("*********************************")
 
+
+def carrega_palavra_secreta():
     arquivo = open("palavras.txt", "r")
     # abre o arquivo na variável no modo "r" leitura
     palavras = []
@@ -25,48 +66,114 @@ def jogar():
 
     palavra_secreta = palavras[numero].upper()
     # pega o index da palavra na lista e transforma a palavra para maiúsculo
+    return palavra_secreta
 
-    letras_acertadas = ["_" for letra in palavra_secreta]
-    # array, lista com as letras acertadas pelo usuário
-    # cria uma lista e
-    # adiciona dinamicamente de acordo com o tamanho da palavra
-    # Funcionalidade List Comprehensions
 
-    erros = 0
+def inicializa_letras_acertadas(palavra):
+    return ["_" for letra in palavra]
 
-    while (True):
-        chute = input("Qual letra? ")
-        # trata a string removendo espaços do input do usuário
-        # upper() transforma o input para maiúsculo
-        chute = chute.strip().upper()
 
-        if (chute in palavra_secreta):
-            index = 0
-            # devolve a posição da letra encontrada na palavra secreta
-            for letra in palavra_secreta:
-                if (chute == letra):
-                    letras_acertadas[index] = letra
-                index += 1  # index = index + 1
+def pede_chute():
+    chute = input("Qual letra? ")
+    # trata a string removendo espaços do input do usuário
+    # upper() transforma o input para maiúsculo
+    chute = chute.strip().upper()
+    return chute
 
-        else:
-            erros += 1  # erros = erros + 1
-            print("EROU! {} tentativas restantes.".format(6-erros))
 
-        if (erros == 6):
-            break
-        if ("_" not in letras_acertadas):
-            print(letras_acertadas)
-            break
+def acertou_chute(chute, letras_acertadas, palavra_secreta):
+    index = 0
+    # devolve a posição da letra encontrada na palavra secreta
+    for letra in palavra_secreta:
+        if (chute == letra):
+            letras_acertadas[index] = letra
+        index += 1  # index = index + 1
 
-        print(letras_acertadas)
 
-    if ("_" not in letras_acertadas):
-        print("Você ganhou!")
+def imprime_mensagem_vitoria():
+    print("Parabéns, você ganhou!")
+    print("       ___________      ")
+    print("      '._==_==_=_.'     ")
+    print("      .-\\:      /-.    ")
+    print("     | (|:.     |) |    ")
+    print("      '-|:.     |-'     ")
+    print("        \\::.    /      ")
+    print("         '::. .'        ")
+    print("           ) (          ")
+    print("         _.' '._        ")
+    print("        '-------'       ")
 
-    else:
-        print("Você perdeu!")
 
-    print("Fim do jogo")
+def imprime_mensagem_derrota(palavra_secreta):
+    print("Puxa, você foi enforcado!")
+    print("A palavra era {}".format(palavra_secreta))
+    print("    _______________         ")
+    print("   /               \       ")
+    print("  /                 \      ")
+    print("//                   \/\  ")
+    print("\|   XXXX     XXXX   | /   ")
+    print(" |   XXXX     XXXX   |/     ")
+    print(" |   XXX       XXX   |      ")
+    print(" |                   |      ")
+    print(" \__      XXX      __/     ")
+    print("   |\     XXX     /|       ")
+    print("   | |           | |        ")
+    print("   | I I I I I I I |        ")
+    print("   |  I I I I I I  |        ")
+    print("   \_             _/       ")
+    print("     \_         _/         ")
+    print("       \_______/           ")
+
+
+def desenha_forca(erros):
+    print("  _______     ")
+    print(" |/      |    ")
+
+    if (erros == 1):
+        print(" |      (_)   ")
+        print(" |            ")
+        print(" |            ")
+        print(" |            ")
+
+    if (erros == 2):
+        print(" |      (_)   ")
+        print(" |      \     ")
+        print(" |            ")
+        print(" |            ")
+
+    if (erros == 3):
+        print(" |      (_)   ")
+        print(" |      \|    ")
+        print(" |            ")
+        print(" |            ")
+
+    if (erros == 4):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |            ")
+        print(" |            ")
+
+    if (erros == 5):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |            ")
+
+    if (erros == 6):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      /     ")
+
+    if (erros == 7):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      / \   ")
+
+    print(" |            ")
+    print("_|___         ")
+    print()
 
 
 # verifica se o arquivo foi chamado diretamente, se sim, executa a função jogar()
